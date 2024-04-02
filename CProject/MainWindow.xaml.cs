b2cc48ac213a7de0a33e888437e79768a00cd972
Pasha <FirAnorselLia@gmail.com>
2024-04-02 20:53:57 +0300

    Финальная версия

--- CProject/MainWindow.xaml.cs
+++ CProject/MainWindow.xaml.cs
@@ -23,20 +23,11 @@ namespace CProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        Context db;
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = new MainViewModel();
-
-            this.Closing += MainWindow_Closing;
-        }
-
-
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            db.Dispose();
         }
     }
 }
